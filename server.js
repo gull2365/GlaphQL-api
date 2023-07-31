451e29188b9d329b2bf8eac9de190678b17f0e85
Gull 2365 <jych2365@gmail.com>
2023-07-31 23:00:51 +0900

    feat: Mutation Type

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const typeDefs = gql`
   type Query {
     allTweets: [Tweet]
     tweet(id: ID): Tweet
+  }
+  type Mutation {
+    postTweet(text: String, userId: ID): Tweet
+    deleteTweet(id: ID): Boolean
+  }
 `;
 
 const server = new ApolloServer({ typeDefs });
