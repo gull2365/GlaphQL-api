2e15519b14fd5c2ea1d89cc7cb2e3f32811cfdd5
Gull 2365 <jych2365@gmail.com>
2023-08-03 11:46:56 +0900

    feat: Query Resolvers

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  {
+    id: "1",
+    text: "first",
+  },
+  {
+    id: "2",
+    text: "hello",
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID
@@ -10,19 +21,31 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
   }
+
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
     deleteTweet(id: ID!): Boolean!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return this.tweet.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
